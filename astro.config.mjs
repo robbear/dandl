8f115fee3c4fb527941b61d36b0df1fe7cb59700
Rob Bearman <robbear@hyperfine.com>
2025-10-08 11:11:17 -0700

    Redo main page and reorg into ethics, government and science

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      starlight({
-          title: 'My Docs',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-          ],
-      }),
-	],
+    starlight({
+      title: "Decency and Learning",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Ethics",
+          autogenerate: { directory: "ethics" },
+        },
+        {
+          label: "Government",
+          autogenerate: { directory: "government" },
+        },
+        {
+          label: "Science",
+          autogenerate: { directory: "science" },
+        },
+      ],
+    }),
+  ],
 
   adapter: vercel(),
 });
