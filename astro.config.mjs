9160eb48ad783171f9a6b348cbe99e46a45a8b51
Rob Bearman <robbear@hyperfine.com>
2025-10-08 13:39:12 -0700

    Add site attribute to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://decencyandlearning.com",
   integrations: [
     starlight({
       title: "Decency and Learning",
